78a2af77db8438260b823d2b248b992fe76b74c2
Jasper Smit <git@jrhrsmit.nl>
2023-06-28 08:30:37 +0200

    add parallelism in fetch.sh for jlcpcb db

--- jlcpcb_part_database/fetch.sh
+++ jlcpcb_part_database/fetch.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-wget https://yaqwsx.github.io/jlcparts/data/cache.zip
+function fetch_main () {
+    url="https://yaqwsx.github.io/jlcparts/data/cache.zip"
+    rm -f "$url"
+    wget "$url"
+}
+function fetch_part () {
+    url="https://yaqwsx.github.io/jlcparts/data/cache.z0$i"
+    rm -f "$url"
+    wget "$url"
+}
+
+fetch_main &
 for i in {1..7}; do
-	wget "https://yaqwsx.github.io/jlcparts/data/cache.z0$i"
+    fetch_part $i &
 done
+
+wait
 7z x cache.zip
